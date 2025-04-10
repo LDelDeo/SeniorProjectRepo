566c4854c9f476c0e75e576ebab0308de25a6ac2
LDelDeo <ljdeldeo@email.NEIT.edu>
2025-04-10 16:12:21 -0400

    Reloading

--- SeniorProject2025/Assets/Scripts/Player/FPShooting.cs
+++ SeniorProject2025/Assets/Scripts/Player/FPShooting.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
 
 public class FPShooting : MonoBehaviour
 {
@@ -11,12 +14,29 @@ public class FPShooting : MonoBehaviour
     [Header("Gameplay")]
     private bool isBlocking;
 
+    [Header("Shooting & Reloading")]
+    private float timeToReload = 2.5f;
+    private bool hasAmmo;
+    private int bullets;
+    public TMP_Text bulletsText;
+    public TMP_Text reloadText;
+
+    private void Start()
+    {
+        hasAmmo = true;
+        bullets = 16;
+        reloadText.text = "";
+    }
+
     private void Update()
     {
+        bulletsText.text = "" + bullets;
+
         // Left Click to Shoot
         if (Input.GetMouseButtonDown(0))
         {
             Shoot();
+            gunAnim.Play("Recoil");
         }
 
         // Right Click to Shield
@@ -29,16 +49,33 @@ public class FPShooting : MonoBehaviour
         {
             Unblock();
         }
+
+        // R to Reload
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reload();
+        }
+
+        if (bullets <= 0)
+        {
+            hasAmmo = false;
+            reloadText.text = "R to Reload";
+        }
+        else
+        {
+            reloadText.text = "";
+        }
     }
     private void Shoot()
     {
-        if (!isBlocking)
+        if (!isBlocking && hasAmmo)
         {
             Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-            gunAnim.Play("Recoil");
+            bullets--;
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 Debug.Log("Hit " + hit.collider.name);
+                
 
                 if (hit.collider.tag == "MeleeOrcEnemy")
                 hit.collider.GetComponent<MeleeOrcEnemy>().TakeDamage(playerStats.playerRangedDamage);
@@ -50,6 +87,19 @@ public class FPShooting : MonoBehaviour
             }
         }
         
+    }
+
+    private void Reload()
+    {
+        //Play Animation Here
+        StartCoroutine(Reloading());
+    }
+
+    private IEnumerator Reloading()
+    {
+        yield return new WaitForSeconds(timeToReload);
+        hasAmmo = true;
+        bullets = 16;
     }
 
     private void Block()
