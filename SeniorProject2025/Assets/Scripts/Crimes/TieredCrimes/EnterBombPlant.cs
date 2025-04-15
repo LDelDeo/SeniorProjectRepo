d7ca4b070ab6890b620f0134e81e10ccec229494
LDelDeo <ljdeldeo@email.NEIT.edu>
2025-04-15 17:47:02 -0400

    Bomb Countdown

--- SeniorProject2025/Assets/Scripts/Crimes/TieredCrimes/EnterBombPlant.cs
+++ SeniorProject2025/Assets/Scripts/Crimes/TieredCrimes/EnterBombPlant.cs
@@ -1,37 +1,71 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
 
 public class EnterBombPlant : MonoBehaviour
 {
     public GameObject[] enemies;
     public GameObject exclamationPoint;
 
+    // Bomb Countdown
+    public TMP_Text countdownText;
+    private float countdownTime = 60f;
+    private bool timerStarted = false;
+    private bool timerEnded = false;
+
     [Header("Script Grabs")]
     private CrimeCompletion crimeCompletion;
     private WireCut wireCut;
 
-    public void Update()
+    private void Start()
     {
+        countdownText.text = "";
+    }
+
+    void Update()
+{
+    if (crimeCompletion == null)
         crimeCompletion = FindObjectOfType<CrimeCompletion>();
 
-        if (wireCut == null)
+    if (wireCut == null)
         wireCut = FindObjectOfType<WireCut>();
+
+    if (timerStarted && !timerEnded)
+    {
+        countdownTime -= Time.deltaTime;
+        countdownText.text = Mathf.CeilToInt(countdownTime).ToString();
+
+        if (countdownTime <= 0f)
+        {
+            countdownText.text = "Too Late!";
+            wireCut.doneCorrectly = false;
+
+            Debug.Log("Crime Failed: Timer ran out.");
+
+            Destroy(exclamationPoint);
+            Destroy(gameObject);
+            timerEnded = true;
+        }
 
         if (wireCut.doneCorrectly)
         {
             Debug.Log("Crime Stopped!");
-
-            // Payout Player Credits
             crimeCompletion.CrimeStopped(crimeCompletion.tierThreeXP, crimeCompletion.tierThreeCredits);
 
             Destroy(exclamationPoint);
             Destroy(gameObject);
+            timerEnded = true;
         }
     }
+}
+
 
     private void OnTriggerEnter(Collider other) 
     {
         if (other.CompareTag("Player"))
         {
+            timerStarted = true;
 
             foreach (Transform child in transform)
             {
