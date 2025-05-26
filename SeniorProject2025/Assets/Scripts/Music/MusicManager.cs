87ba31895b7171a07f83b1e13f69104fb45da608
LDelDeo <ljdeldeo@email.NEIT.edu>
2025-05-26 11:14:15 -0400

    Music Script

---
+++ SeniorProject2025/Assets/Scripts/Music/MusicManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    [Header("Music Tracks")]
+    public AudioClip suburbs;
+    public AudioClip rich;
+    public AudioClip hood;
+    public AudioClip industrial;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
